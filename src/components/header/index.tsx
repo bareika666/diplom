758b563a184d62ca923405b5dcdd5d95ef32a177
Rinal <bariyev_r@inbox.ru>
2024-03-19 19:03:34 +0300

    коментарий

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,3 +1,17 @@
-const Header = () => {
-  return <header></header>;
+import styled from "styled-components";
+import { Logo } from "./img";
+
+const HeaderWrapper = styled.div`
+display:flex;
+align-items:center;
+background-color: #18311B;
+
+`;
+
+
+
+export const Header = () => {
+  return <HeaderWrapper>
+      <Logo/>
+  </HeaderWrapper>;
 };
